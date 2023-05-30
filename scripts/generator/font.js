412631ef4d7277ec2e5fbcfbd605191813a9af9c
hozen <hozen@live.com>
2023-05-30 20:46:24 +0800

    fix(control): fix a bug that causes some custome fonts not display properly

--- scripts/generator/font.js
+++ scripts/generator/font.js
@@ -19,7 +19,7 @@ module.exports = function (hexo) {
     } = hexo.theme.config.custom_font
 
     const folder = path.resolve(__dirname, "../../", fontFolder)
-    const text = getHanText(hexo)
+    const text = getSubText(hexo)
     console.log("Extract subfont: ", text)
 
     return subFamily.map(font => {
@@ -50,16 +50,30 @@ function compress(text, params) {
   return Buffer.from(res)
 }
 
-function getHanText(hexo) {
+function getSubText(hexo) {
   const config = hexo.theme.config
-  const text = [config.slogan]
+  let text = [config.slogan]
     .concat(config.subpage.pages.map(p => p.description))
     .concat(hexo.locals.get('tags').map(tag => tag.name))
     .concat(config.index.about.text)
     .concat(config.index.poem)
     .concat(config.foot.title)
     .join("").split("")
-    // .filter(rune => /[\u4e00-\u9fa5]/.test(rune))
+  text = handleAscii(Array.from(new Set(text)))
+  text.push(" ")
+
+  // .filter(rune => /[\u4e00-\u9fa5]/.test(rune))
   // must be sorted and .notdef at first position. see: https://github.com/opentypejs/opentype.js/issues/94
-  return Array.from(new Set(text)).sort().join("")
+  return text.sort().join("")
+}
+
+function handleAscii(text) {
+  const ascii = text.filter(w => 0x21 <= w.charCodeAt() && w.charCodeAt() <= 0x7E)
+  const capital = ascii.filter(w => 0x41 <= w.charCodeAt() && w.charCodeAt() <= 0x5A)
+  const lowcase = ascii.filter(w => 0x61 <= w.charCodeAt() && w.charCodeAt() <= 0x7A)
+  const res = text
+    .concat(ascii.map(w => String.fromCharCode(w.charCodeAt() + 0xFEE0)))
+    .concat(capital.map(w => String.fromCharCode(w.charCodeAt() + 0x20)))
+    .concat(lowcase.map(w => String.fromCharCode(w.charCodeAt() - 0x20)))
+  return Array.from(new Set(res))
 }
