6022b861a5a80d00b825c1b4c7c26397cdd183cb
hozenli <hozen@tencent.com>
2024-09-22 13:54:46 +0800

    feat nav sticky

--- source/js/copy-code.js
+++ source/js/copy-code.js
@@ -1,29 +1,28 @@
-const getCopyButton = () => {
-  const button = document.createElement("div")
-  button.innerHTML = `复制代码`
-  button.className = 'copy-button'
-  return button
-}
-
-const codeBlocks = document.querySelectorAll('figure.highlight')
-
-console.log(codeBlocks)
-
-
-codeBlocks.forEach((codeBlock) => {
-  const copyButton = getCopyButton();
-  copyButton.onclick = () => {
-    try {
-      const code = codeBlock.querySelector('code').innerText
-      navigator.clipboard.writeText(code);
-      copyButton.innerText = '已复制!'
-    } catch {
-      copyButton.innerText = '发生错误'
-    } finally {
-      setTimeout(() => {
-        copyButton.innerText = '复制代码'
-      }, 1000)
-    }
+document.addEventListener('DOMContentLoaded', () => {
+  const getCopyButton = () => {
+    const button = document.createElement("div")
+    button.innerHTML = `复制代码`
+    button.className = 'copy-button'
+    return button
   }
-  codeBlock.appendChild(copyButton)
+
+  const codeBlocks = document.querySelectorAll('figure.highlight')
+
+  codeBlocks.forEach((codeBlock) => {
+    const copyButton = getCopyButton();
+    copyButton.onclick = () => {
+      try {
+        const code = codeBlock.querySelector('code').innerText
+        navigator.clipboard.writeText(code);
+        copyButton.innerText = '已复制!'
+      } catch {
+        copyButton.innerText = '发生错误'
+      } finally {
+        setTimeout(() => {
+          copyButton.innerText = '复制代码'
+        }, 1000)
+      }
+    }
+    codeBlock.appendChild(copyButton)
+  })
 })
