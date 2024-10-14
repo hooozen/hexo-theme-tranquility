174c2acb003f5bf1812e7bc9d8b82340c0ec17de
hozenli <hozen@tencent.com>
2024-10-14 22:51:27 +0800

    feat: english support

--- source/js/copy-code.js
+++ source/js/copy-code.js
@@ -1,28 +1,40 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const getCopyButton = () => {
-    const button = document.createElement("div")
-    button.innerHTML = `复制代码`
-    button.className = 'copy-button'
-    return button
-  }
+const copyText = {
+  get copy() {
+    return document.documentElement.lang === "en" ? "copy" : "复制代码";
+  },
+  get copyError() {
+    return document.documentElement.lang === "en" ? "error" : "发生错误";
+  },
+  get copied() {
+    return document.documentElement.lang === "en" ? "copied!" : "已复制";
+  },
+};
 
-  const codeBlocks = document.querySelectorAll('figure.highlight')
+document.addEventListener("DOMContentLoaded", () => {
+  const getCopyButton = () => {
+    const button = document.createElement("div");
+    button.innerHTML = copyText.copy;
+    button.className = "copy-button";
+    return button;
+  };
+
+  const codeBlocks = document.querySelectorAll("figure.highlight");
 
   codeBlocks.forEach((codeBlock) => {
     const copyButton = getCopyButton();
     copyButton.onclick = () => {
       try {
-        const code = codeBlock.querySelector('code').innerText
+        const code = codeBlock.querySelector("code").innerText;
         navigator.clipboard.writeText(code);
-        copyButton.innerText = '已复制!'
+        copyButton.innerText = copyText.copied;
       } catch {
-        copyButton.innerText = '发生错误'
+        copyButton.innerText = copyText.copyError;
       } finally {
         setTimeout(() => {
-          copyButton.innerText = '复制代码'
-        }, 1000)
+          copyButton.innerText = copyText.copy;
+        }, 1000);
       }
-    }
-    codeBlock.appendChild(copyButton)
-  })
-})
+    };
+    codeBlock.appendChild(copyButton);
+  });
+});
